4d49f742fb62e0d47763ab24dd1c83fe30630dae
WilliamTatendaJose <40793621+WilliamTatendaJose@users.noreply.github.com>
2024-08-17 19:54:51 +0200

    vercil

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -57,13 +57,7 @@ const HeroSection = () => {
               </div>
             </div>
           </div>
-          <div className="md:w-1/2 mt-8 md:mt-0">
-            <img 
-              src="./src/assets/close-up-circuit-board-with-copy-space.jpg" 
-              alt="Electronics Repair" 
-              className="rounded-lg shadow-xl"
-            />
-          </div>
+        
         </div>
       </div>
         </div>
